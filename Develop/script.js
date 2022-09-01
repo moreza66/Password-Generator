550673adda1d25df638b564c8ab80562ba96d3e0
mohammadreza memar <mohammadrezamemar@mohammarezasMBP.attlocal.net>
2022-09-01 14:01:02 -0500

    added randomize functions

--- Develop/script.js
+++ Develop/script.js
@@ -2,6 +2,20 @@
 
 var generateBtn = document.querySelector("#generate");
 
+function randomInt(min, max) {
+  if (!max) {
+    max = min
+    min = 0
+  }
+  var rand = Math.random()
+  return Math.floor(min*(1 - rand)+ rand*max)
+}
+
+function getRandomItem(list) {
+  return list[randomInt(list.length)]
+}
+
+
 
 // Write password to the #password input
 function writePassword() {
