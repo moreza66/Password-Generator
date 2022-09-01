68dc52e05f989ac49f7d75c85051565fd052edfd
mohammadreza memar <mohammadrezamemar@mohammarezasMBP.attlocal.net>
2022-09-01 13:55:35 -0500

    start working on JS

--- Develop/script.js
+++ Develop/script.js
@@ -1,15 +1,19 @@
 // Assignment code here
 
-
-// Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
+
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  if (password) {
+    passwordText.value = password;
+  }
+    
+
 
 }
 
