d25208d43f159a0db68b0f6fb1e91968ec29bc85
mohammadreza memar <mohammadrezamemar@mohammarezasMBP.attlocal.net>
2023-01-05 10:02:43 -0600

    changed the code and readme.md added

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
-# password-generator
+# Randomize Password Generator
+
+![Github licence](http://img.shields.io/badge/license-MIT-blue.svg)
+
+## Description
+
+This project uses JavaScript to generate a random password. Criteria: Password length must be between 8 to 128 characters. User can choose lowercase, uppercase, number and special characters.
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+
+The user should clone the repository from GitHub. This application requires Node.js, Express.js, and Sequelize. To connect to the database run mysql -u root -p and enter password from .env file. Then source the schema.sql. To seed the file run npm run seed. Finally to connect to the server run npm start.
+No installation required.
+
+## Usage
+
+This application will allow users generate a random password based on the criteria they select.
+![](./assests/Image/Screen%20Shot.jpg)
+
+## License
+
+This project is license under MIT
+
+## Contributing
+
+Contributors should read the installation section.
+
+## Tests
+
+There are no tests for this application.
+
+## Questions
+
+If you have any questions about this projects, please contact me directly at moreza.memar@gmail.com. You can view more of my projects at https://github.com/moreza66.
--- assests/script.js
+++ assests/script.js
@@ -1,96 +1,63 @@
-// Assignment code here
 
-var generateBtn = document.querySelector("#generate");
+// Password Criteria
+let pwLength
+const loweCase = "abcdefhijklmnopqrstuvwxyz";
+const uperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const number = "0123456789";
+const specialCharacter = "!#$%&*+-=?@^_~"
 
-function randomInt(min, max) {
-  if (!max) {
-    max = min
-    min = 0
+// Generate Password Function
+const generatePassword = function () {
+
+  // Password length between 8 - 128 characters 
+  pwLength = parseInt(window.prompt("Please enter desired password length between 8 - 128 characters"));
+  while (pwLength < 8 || pwLength > 128) {
+    alert("Please follow the instructions.");
+    pwLength = parseInt(window.prompt("Please enter desired password length between 8 - 128 characters"));
+  };
+
+ // Choose lowercase, uppercase, numeric, and/or special characters 
+ // uperrcase
+  const uppercase = window.confirm("Would you like your password to include lowercase letters?");
+  // lowercase
+  const lowercase = window.confirm("Would you like your password to include upercase letters?");
+  // Numbers
+  const numbers = window.confirm("Would you like your password to include numbers letters?");
+  //pecialCharacter
+  const sChars = window.confirm("Would you like your password to include special characters letters?");
+
+  let pw = '';
+
+  if (lowercase == true) {
+    pw += loweCase;
   }
-  var rand = Math.random()
-  return Math.floor(min*(1 - rand)+ rand*max)
+  if (uppercase == true) {
+    pw += uperCase;
+  }
+  if (numbers == true) {
+    pw += number;
+  }
+  if (sChars == true) {
+    pw += specialCharacter;
+  }
+  
+  // Password For Loop
+  let password = "";
+  for (let i = 0; i < pwLength; i++) {
+    password += pw.charAt(Math.floor(Math.random() * pw.length));
+  }
+  return password;
 }
 
-function getRandomItem(list) {
-  return list[randomInt(list.length)]
-}
+// Get references to the #generate element
+const generateBtn = document.querySelector("#generate");
 
-function generatePassword() {
-
-  while (true) {
-    var userInput = window.prompt("How long do you want your password to be?")
-    // user exited the prompt
-    if (userInput === null) {
-      return
-    }
-  
-    var passwordLength = parseInt(userInput)
-  
-    if (isNaN(passwordLength)) {
-      window.alert("That's not a number!")
-  } else if (passwordLength <8 || passwordLength > 128) {
-    window.alert("invalid password lenght. lenght should be between 8 and 128 characters.")
-   } else {
-      break
-    }
-  }
-  
-  var userWantsNumbers = window.confirm("would you like to include numbers on your password?")
-  var userWantsSymbols = window.confirm("would you like to include Symbols on your password?")
-  var userWantLowercase = window.confirm("would you like to include Lowercase on your password?")
-  var userWantsUppercase = window.confirm("would you like to include Uppercase on your password?")
-  
-  var numberList = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-  var symbollist = [ "!", "@", "#", "$", "%", "^", "&", "*", "?", "=", "-", "+","_","."]
-  var Lowercaselist = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" ]
-  var Uppercaselist = []
-  
-  var optionsCart = []
-  
-  for (var i = 0; i < Lowercaselist.length ; i++) {
-    Uppercaselist[i] = Lowercaselist[i].toUpperCase()
-  }
-  
-  if (userWantsNumbers) optionsCart.push(numberList)
-  
-  if (userWantsSymbols) optionsCart.push(symbollist)
-  
-  if (userWantsUppercase)optionsCart.push(Uppercaselist)
-  
-  if (userWantLowercase) optionsCart.push(Lowercaselist)
-  
-  if (userWantLowercase === true) {
-    optionsCart.push(Lowercaselist)
-  }
-  
-  if (optionsCart.length === 0) {
-    optionsCart.push(Lowercaselist)
-  }
-  
-  var generatedPassword = ""
-  
-  for (var i = 0; i < passwordLength; i++) {
-    var randonlist = getRandomItem(optionsCart)
-    var randonChar = getRandomItem(randonlist)
-    generatedPassword += randonChar
-  }
-  
-  return generatedPassword
-  
-  
-  }
-  
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  const password = generatePassword();
+  const passwordText = document.querySelector("#password");
 
-  var passwordText = document.querySelector("#password");
-
-  if (password) {
-    passwordText.value = password;
-  }
-    
-
+  passwordText.value = password;
 
 }
 
